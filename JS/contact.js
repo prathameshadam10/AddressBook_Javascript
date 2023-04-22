2b57075d7489047aae39eb238048a9136fd8dd18
prathameshadam10 <121919043+prathameshadam10@users.noreply.github.com>
2023-04-23 00:23:04 +0530

    Update contact.js

--- JS/contact.js
+++ JS/contact.js
@@ -43,34 +43,71 @@ class Contact {
      }
      
      set firstName(firstName){
-        return this.firstName = firstName;
+        let firstNameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if(firstNameRegex.test(firstName))
+        this.firstName = firstName;
+        else
+        throw "First Name is Incorrect!!!!!!!!";
+        
      }
      set lastName(lastName){
-        return this.lastName = lastName;
+        let lastNameRegex = RegExp('^[A-Z]{1}[a-z]{3,}$');
+        if(lastNameRegex.test(lastName))
+        this.lastName = lastName;
+        else
+        throw "Last Name is Incorrect!!!!!!!!";
      }
      set address(address){
-        return this.address = address;
+        let addressRegex = RegExp('^[a-zA-Z0-9#,]{4,}$');
+        if(addressRegex.test(address))
+        this.address = address;
+        else
+        throw "Address is Incorrect!!!!!!!!";
      }
       set city(city){
-        return this.city = city;
+        let cityRegex = RegExp('^[a-zA-Z]{3,}$');
+        if(cityRegex.test(city))
+        this.city = city;
+        else
+        throw "City is Incorrect!!!!!!!!";
      }
      set state(state){
-        return this.state = state;
+        let stateRegex = RegExp('^[a-zA-Z]{3,}$');
+        if(stateRegex.test(state))
+        this.state = state;
+        else
+        throw "State is Incorrect!!!!!!!!";
      }
      set zip(zip){
-        return this.zip = zip;
+        let zipRegex = RegExp('^[0-9]{3}\\s{0,1}[0-9]{3}$');
+        if(zipRegex.test(zip))
+        this.zip = zip;
+        else
+        throw "Zip Code is Incorrect!!!!!!!!";
      }
      set phone(phone){
-        return this.phone = phone;
+        let phoneRegex = RegExp('^[0-9]{2}\\s{1}[0-9]{10}$');
+        if(phoneRegex.test(phone))
+        this.phone = phone;
+        else
+        throw "Phone is Incorrect!!!!!!!!";
      }
      set email(email){
-        return this.email = email;
+        let emailRegex = RegExp('^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$');
+        if(emailRegex.test(email))
+        this.email = email;
+        else
+        throw "Email is Incorrect!!!!!!!!";
      }
      toString(){
         return "First Name : "+ this.firstName + ", Last Name : "+ this.lastName + ", Address : " + this.address + ", City : "+ this.city + ", State : "+ this.state +", Zip : "+ this.zip+ ", Phone Number : "+ this.phone + ", Email : "+ this.email;
     }
 }
 
+    try {
     let contact = new Contact("Prathamesh", "Adam", "Kuchan Nagar", "Solapur", "Maharastra", "413005", "8983930906", "prathamadam10@gmail.com" )
      console.log(contact.toString());
-     
+    }
+    catch(e){
+        console.log(e)
+    }
